db8e8686ccef1c1391e36b8d32a8e9554b253901
david perry <perryd11016@gmail.com>
2015-03-26 09:16:39 -0700

    eesese gttgtgtt

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -7,14 +7,16 @@ game.PlayerEntity = me.Entity.extend({
                 spritewidth: "64",
                 spriteheight: "64",
                 getShape: function(){
-                    return(new me.Rect(0, 0, 64, 64 )).toPolygon();
+                    return(new me.Rect(0, 0, 100, 70)).toPolygon();
                 }
         }]); 
     
     this.body.setVelocity(5, 20);
+    me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
     
     this.renderable.addAnimation("idle", [78]);
     this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
+    this.renderable.addAnimation("attack", [65, 66, 67, 68, 69, 70, 71, 72], 80);
     
     this.renderable.setCurrentAnimation("idle");
     },  
@@ -29,14 +31,41 @@ game.PlayerEntity = me.Entity.extend({
         }else{
             this.body.vel.x = 0;
         }
-    if(this.body.vel.x !== 0){
+        
+ 
+          if(me.input.isKeyPressed("attack")){
+           
+            if(!this.renderable.isCurrentAnimation("attack")){
+                console.log(!this.renderable.isCurrentAnimation("attack"));
+                //Sets the current animation to attack and once that is over
+                //goes back to the idle animation
+                this.renderable.setCurrentAnimation("attack", "idle");
+               //Makes it so that the next time we start this sequence we begin
+               //from the first animation, not wherever we left off when we 
+               //switched to another animation
+                this.renderable.setAnimationFrame();
+            }
+        }
+    else if(this.body.vel.x !== 0){
     if(!this.renderable.isCurrentAnimation("walk")){
         this.renderable.isCurrentAnimation("walk");
     }
     }else{
         this.renderable.setCurrentAnimation("idle");    
     }
-      
+             if(me.input.isKeyPressed("attack")){
+           
+            if(!this.renderable.isCurrentAnimation("attack")){
+                console.log(!this.renderable.isCurrentAnimation("attack"));
+                //Sets the current animation to attack and once that is over
+                //goes back to the idle animation
+                this.renderable.setCurrentAnimation("attack", "idle");
+               //Makes it so that the next time we start this sequence we begin
+               //from the first animation, not wherever we left off when we 
+               //switched to another animation
+                this.renderable.setAnimationFrame();
+            }
+        }
       
         this.body.update(delta);
        
@@ -55,20 +84,25 @@ game.PlayerBaseEntity =  me.Entity.extend({
                 spritewidth: "100",
                 spriteheight: "100",
                 getShape: function(){
-                    return (new me.Rect(0, 0, 100, 100)).toPolygon();
+                    return (new me.Rect(0, 0, 100, 70)).toPolygon();
                 }
         }]);
         this.broken = false;
         this.health = 10;
         this.alwaysUpdate = true;
         this.body.onCollision = this.onCollision.bind(this);
-        
+        console.log("init");
         this.type = "PlayerBaseEntity";
+        
+         this.renderable.addAnimation("idle", [0]);
+         this.renderable.addAnimation("broken", [1]);
+         this.renderable.setCurrentAnimation("idle");
     },
     
     update:function(){
       if(this.health<=0){
           this.broken = true;
+          this.renderable.setCurrentAnimation("broken");
       }  
       this.body.update(delta);
       
@@ -101,11 +135,16 @@ game.EnemyBaseEntity =  me.Entity.extend({
         this.body.onCollision = this.onCollision.bind(this);
         
         this.type = "EnemyBaseEntity";
+        
+         this.renderable.addAnimation("idle", [0]);
+         this.renderable.addAnimation("broken", [1]);
+         this.renderable.setCurrentAnimation("idle");
     },
     
     update:function(){
       if(this.health<=0){
           this.broken = true;
+          this.renderable.setCurrentAnimation("broken");
       }  
       this.body.update(delta);
       
--- js/screens/play.js
+++ js/screens/play.js
@@ -11,6 +11,9 @@ game.PlayScreen = me.ScreenObject.extend({
                me.game.world.addChild(player, 5);
                
                me.input.bindKey(me.input.KEY.RIGHT, "right");
+               me.input.bindKey(me.input.KEY.LEFT, "left");
+               me.input.bindKey(me.input.KEY.SPACE, "jump");
+               me.input.bindKey(me.inpu.KEY.A, "attack");
                
 		// add our HUD to the game world
 		this.HUD = new game.HUD.Container();
