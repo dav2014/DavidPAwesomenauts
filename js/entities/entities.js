5c64fc6036da7f4f557686a62951bf412faf98a9
david perry <perryd11016@gmail.com>
2015-03-30 10:11:53 -0700

    gygyggygyg gygygygygygygyg

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -28,10 +28,17 @@ game.PlayerEntity = me.Entity.extend({
            //me.timer.tick makes the movement look smooth
             this.body.vel.x -= this.body.accel.x * me.timer.tick;
             this.flipX(true);
+        }else if(me.input.isKeyPressed("left")){
+            this.body..vel.x -=this.body.accel.x * me.timer.tick;
+            this.flipX(false);
         }else{
             this.body.vel.x = 0;
         }
         
+        if(me.input.isKeyPressed("jump") && !this.jumping && !this.falling){
+           this.jumping = true;
+           this.body.vel.y -= this.body.accel.y * me.timer.tick;
+        }
  
           if(me.input.isKeyPressed("attack")){
            
