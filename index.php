9b357a85272ea7a47caf867b7a481a276fc051de
david perry <perryd11016@gmail.com>
2015-04-21 09:10:51 -0700

    hyhy .,.,.,.,.dfddfd

--- index.php
+++ index.php
@@ -28,6 +28,10 @@
 		<script type="text/javascript" src="js/resources.js"></script>
 
 		<script type="text/javascript" src="js/entities/entities.js"></script>
+                <script type="text/javascript" src="js/entities/EnemyBaseEntity.js.js"></script>
+                <script type="text/javascript" src="js/entities/PlayerBaseEntity.js.js"></script>
+                <script type="text/javascript" src="js/entities/GameManager.js.js"></script>
+                <script type="text/javascript" src="js/entities/EnemyCreep.js.js"></script>
 		<script type="text/javascript" src="js/entities/HUD.js"></script>
 
 		<script type="text/javascript" src="js/screens/title.js"></script>
